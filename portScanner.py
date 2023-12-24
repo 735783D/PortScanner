71653c502bd5e808bc80dc497bb57af8d880dbb5
735783D <john.bays-wit@protonmail.com>
2023-12-23 22:28:08 -0600

    Added user input logic for port scanning and timeout

--- portScanner.py
+++ portScanner.py
@@ -4,9 +4,10 @@ import socket  # this import is for the socket library
 			   # https://www.geeksforgeeks.org/socket-in-computer-network/
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(5)
 
-host = "137.74.187.100"  # ipv4 address of hackthissite.org for testing
-port = 80
+host = input("What ipv4 address are you wanting to scan? ")  # ex. 137.74.187.100 ipv4 address of hackthissite.org for testing
+port = int(input("What port number are you wishing to check for openness? "))
 
 
 def port_scanner(port):
